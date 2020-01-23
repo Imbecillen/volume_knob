1e71fe61dc077300aa47b6c0abab5a086dbb82e6
Imbecillen <yo@sebastiangalo.se>
2020-01-23 09:54:09 +0100

    Add files via upload

---
+++ TrinketVolumeKnob_edit_copy.ino
@@ -0,0 +1,309 @@
+// see tutorial at http://learn.adafruit.com/trinket-usb-volume-knob
+
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+
+#include "TrinketHidCombo.h"
+
+#define neopixel 1
+#define PIN_ENCODER_A 0
+#define PIN_ENCODER_B 2
+#define TRINKET_PINx  PINB
+
+static uint8_t enc_prev_pos = 0;
+static uint8_t enc_flags    = 0;
+
+
+
+
+// Change this to be at least as long as your pixel string (too long will work fine, just be a little slower)
+#define PIXELS 16  // Number of pixels in the string
+
+
+#define PIXEL_PORT  PORTB  // Port of the pin the pixels are connected to
+#define PIXEL_DDR   DDRB // Port of the pin the pixels are connected to
+#define PIXEL_BIT   neopixel   // Bit of the pin the pixels are connected to
+
+// These are the timing constraints taken mostly from the WS2812 datasheets 
+// These are chosen to be conservative and avoid problems rather than for maximum throughput 
+
+#define T1H  900    // Width of a 1 bit in ns
+#define T1L  600    // Width of a 1 bit in ns
+
+#define T0H  400    // Width of a 0 bit in ns
+#define T0L  900    // Width of a 0 bit in ns
+
+// The reset gap can be 6000 ns, but depending on the LED strip it may have to be increased
+// to values like 600000 ns. If it is too small, the pixels will show nothing most of the time.
+#define RES 6000    // Width of the low gap between bits to cause a frame to latch
+
+// Here are some convience defines for using nanoseconds specs to generate actual CPU delays
+
+#define NS_PER_SEC (1000000000L)          // Note that this has to be SIGNED since we want to be able to check for negative values of derivatives
+
+
+
+#define CYCLES_PER_SEC (F_CPU)
+
+#define NS_PER_CYCLE ( NS_PER_SEC / CYCLES_PER_SEC )
+
+#define NS_TO_CYCLES(n) ( (n) / NS_PER_CYCLE )
+
+// Actually send a bit to the string. We must to drop to asm to enusre that the complier does
+// not reorder things and make it so the delay happens in the wrong place.
+
+inline void sendBit( bool bitVal ) {
+  
+    if (  bitVal ) {        // 0 bit
+      
+    asm volatile (
+      "sbi %[port], %[bit] \n\t"        // Set the output bit
+      ".rept %[onCycles] \n\t"                                // Execute NOPs to delay exactly the specified number of cycles
+      "nop \n\t"
+      ".endr \n\t"
+      "cbi %[port], %[bit] \n\t"                              // Clear the output bit
+      ".rept %[offCycles] \n\t"                               // Execute NOPs to delay exactly the specified number of cycles
+      "nop \n\t"
+      ".endr \n\t"
+      ::
+      [port]    "I" (_SFR_IO_ADDR(PIXEL_PORT)),
+      [bit]   "I" (PIXEL_BIT),
+      [onCycles]  "I" (NS_TO_CYCLES(T1H) - 2),    // 1-bit width less overhead  for the actual bit setting, note that this delay could be longer and everything would still work
+      [offCycles]   "I" (NS_TO_CYCLES(T1L) - 2)     // Minimum interbit delay. Note that we probably don't need this at all since the loop overhead will be enough, but here for correctness
+
+    );
+                                  
+    } else {          // 1 bit
+
+    // **************************************************************************
+    // This line is really the only tight goldilocks timing in the whole program!
+    // **************************************************************************
+
+
+    asm volatile (
+      "sbi %[port], %[bit] \n\t"        // Set the output bit
+      ".rept %[onCycles] \n\t"        // Now timing actually matters. The 0-bit must be long enough to be detected but not too long or it will be a 1-bit
+      "nop \n\t"                                              // Execute NOPs to delay exactly the specified number of cycles
+      ".endr \n\t"
+      "cbi %[port], %[bit] \n\t"                              // Clear the output bit
+      ".rept %[offCycles] \n\t"                               // Execute NOPs to delay exactly the specified number of cycles
+      "nop \n\t"
+      ".endr \n\t"
+      ::
+      [port]    "I" (_SFR_IO_ADDR(PIXEL_PORT)),
+      [bit]   "I" (PIXEL_BIT),
+      [onCycles]  "I" (NS_TO_CYCLES(T0H) - 2),
+      [offCycles] "I" (NS_TO_CYCLES(T0L) - 2)
+
+    );
+      
+    }
+    
+
+    
+}  
+
+  
+inline void sendByte( unsigned char byte ) {
+    
+    for( unsigned char bit = 0 ; bit < 8 ; bit++ ) {
+      
+      sendBit( bitRead( byte , 7 ) );                // Neopixel wants bit in highest-to-lowest order
+                                                     // so send highest bit (bit #7 in an 8-bit byte since they start at 0)
+      byte <<= 1;                                    // and then shift left so bit 6 moves into 7, 5 moves into 6, etc
+      
+    }           
+} 
+
+
+
+inline void sendPixel( unsigned char r, unsigned char g , unsigned char b )  {  
+  
+  sendByte(g);          // Neopixel wants colors in green then red then blue order
+  sendByte(r);
+  sendByte(b);
+  
+}
+
+
+// Just wait long enough without sending any bots to cause the pixels to latch and display the last sent frame
+
+void show() {
+  _delay_us( (RES / 1000UL) + 1);       // Round up since the delay must be _at_least_ this long (too short might not work, too long not a problem)
+}
+
+
+
+
+// Display a single color on the whole string
+
+void showColor( unsigned char r , unsigned char g , unsigned char b ) {
+  
+  cli();  
+  for( int p=0; p<PIXELS; p++ ) {
+    sendPixel( r , g , b );
+  }
+  sei();
+  show();
+  
+}
+
+
+
+
+
+// RGB color, min 1, max 255
+int rVal = 50; // 
+int gVal = 50; // 
+int bVal = 50; // 
+
+int rDir = -1;
+int gDir = -1;
+int bDir = -1;
+
+int brightness = 200; // maximum 255
+
+
+void setup()
+{
+
+
+ // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+  #if defined (__AVR_ATtiny85__)
+    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+  #endif
+  // End of trinket special code
+  
+
+  DDRB |= (1 << neopixel); //replaces pinMode(PB0, OUTPUT);
+
+  
+  // set pins as input with internal pull-up resistors enabled
+  pinMode(PIN_ENCODER_A, INPUT);
+  pinMode(PIN_ENCODER_B, INPUT);
+
+  PORTB |= (1 << neopixel); //replaces digitalWrite(PB0, HIGH);
+
+  
+  digitalWrite(PIN_ENCODER_A, HIGH);
+  digitalWrite(PIN_ENCODER_B, HIGH);
+
+  TrinketHidCombo.begin(); // start the USB device engine and enumerate
+
+  // get an initial reading on the encoder pins
+  if (digitalRead(PIN_ENCODER_A) == LOW) {
+    enc_prev_pos |= (1 << 0);
+  }
+  if (digitalRead(PIN_ENCODER_B) == LOW) {
+    enc_prev_pos |= (1 << 1);
+  }
+}
+
+void loop() {
+
+showColor(rVal, gVal, bVal);
+
+  rVal = rVal + rDir;
+  gVal = gVal + gDir;
+  bVal = bVal + bDir;
+
+
+//originally 255, 
+   if (rVal >= brightness || rVal <= 0) {
+    rDir = rDir * -1;
+  }
+
+  if (gVal >= brightness || gVal <= 0) {
+    gDir = gDir * -1;
+  }
+
+  if (bVal >= brightness || bVal <= 0) {
+    bDir = bDir * -1;
+  }
+
+  // sets pulsating speed
+  delay(20);
+  
+  return;
+
+
+
+// all under for encoder
+  
+  int8_t enc_action = 0; // 1 or -1 if moved, sign is direction
+
+  // note: for better performance, the code will now use
+  // direct port access techniques
+  // http://www.arduino.cc/en/Reference/PortManipulation
+  uint8_t enc_cur_pos = 0;
+  // read in the encoder state first
+  if (bit_is_clear(TRINKET_PINx, PIN_ENCODER_A)) {
+    enc_cur_pos |= (1 << 0);
+  }
+  if (bit_is_clear(TRINKET_PINx, PIN_ENCODER_B)) {
+    enc_cur_pos |= (1 << 1);
+  }
+
+  // if any rotation at all
+  if (enc_cur_pos != enc_prev_pos)
+  {
+    if (enc_prev_pos == 0x00)
+    {
+      // this is the first edge
+      if (enc_cur_pos == 0x01) {
+        enc_flags |= (1 << 0);
+      }
+      else if (enc_cur_pos == 0x02) {
+        enc_flags |= (1 << 1);
+      }
+    }
+
+    if (enc_cur_pos == 0x03)
+    {
+      // this is when the encoder is in the middle of a "step"
+      enc_flags |= (1 << 4);
+    }
+    else if (enc_cur_pos == 0x00)
+    {
+      // this is the final edge
+      if (enc_prev_pos == 0x02) {
+        enc_flags |= (1 << 2);
+      }
+      else if (enc_prev_pos == 0x01) {
+        enc_flags |= (1 << 3);
+      }
+
+      // check the first and last edge
+      // or maybe one edge is missing, if missing then require the middle state
+      // this will reject bounces and false movements
+      if (bit_is_set(enc_flags, 0) && (bit_is_set(enc_flags, 2) || bit_is_set(enc_flags, 4))) {
+        enc_action = 1;
+      }
+      else if (bit_is_set(enc_flags, 2) && (bit_is_set(enc_flags, 0) || bit_is_set(enc_flags, 4))) {
+        enc_action = 1;
+      }
+      else if (bit_is_set(enc_flags, 1) && (bit_is_set(enc_flags, 3) || bit_is_set(enc_flags, 4))) {
+        enc_action = -1;
+      }
+      else if (bit_is_set(enc_flags, 3) && (bit_is_set(enc_flags, 1) || bit_is_set(enc_flags, 4))) {
+        enc_action = -1;
+      }
+
+      enc_flags = 0; // reset for next time
+    }
+  }
+
+  enc_prev_pos = enc_cur_pos;
+
+  if (enc_action > 0) {
+    TrinketHidCombo.pressMultimediaKey(MMKEY_VOL_UP);
+  }
+  else if (enc_action < 0) {
+    TrinketHidCombo.pressMultimediaKey(MMKEY_VOL_DOWN);
+  }
+  else {
+    TrinketHidCombo.poll(); // do nothing, check if USB needs anything done
+  }
+}
